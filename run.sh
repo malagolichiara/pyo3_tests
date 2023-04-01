58f1418358648485cc7d9ded5cfcabfe25800316
Malagoli Chiara <malagoli@gbinside.com>
2023-04-01 14:16:59 +0100

    add pyo3 parallel version

--- run.sh
+++ run.sh
@@ -7,6 +7,8 @@ new-session \
 'python 2022ex19_plain.py'\; \
 split-window \
 'python 2022ex19_pyo3.py'\; \
+split-window \
+'python 2022ex19_pyo3_parallel.py'\; \
 split-window -h \
 'python 2022ex19_numba.py'\; \
 select-layout even-horizontal\; \
